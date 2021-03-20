4c3c341a878dc8c394771aaf0b4ff52fc14eaefe
AnmolChacra <anmol.chachra@gmail.com>
2021-03-19 21:21:43 -0400

    Update version numbers in setup files

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author='Anmol Chachra',
     author_email='anmol.chachra@gmail.com',
     name='django-location-app',
-    version='1.0.1',
+    version='1.0.3',
     install_requires=['Django>=3.0'],
     python_requires='>=3.7',
     keywords='django geocomplete google maps places',
